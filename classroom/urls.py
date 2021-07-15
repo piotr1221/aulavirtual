69bbf9912b68f3103f47b766bc8d4204bbe70b7b
Jhoan Caramantin <jhoan1915@gmail.com>
2021-07-15 15:01:52 -0500

    fix:Correccion de merge

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path
-<<<<<<< HEAD
 from classroom.views import Categories, CategoryCourses, NewCourse, Enroll, DeleteCourse, EditCourse, MyCourses, CourseDetail, Submissions, StudentSubmissions, GradeSubmission, StudentsNotas, StudentEnrollList,DeleteStundentEnroll,AddStundentEnroll
-=======
-from classroom.views import Categories, CategoryCourses, NewCourse, Enroll, DeleteCourse, EditCourse, MyCourses, CourseDetail, StudentGrades, Submissions, StudentSubmissions, GradeSubmission, StudentsNotas, StudentEnrollList,DeleteStundentEnroll
->>>>>>> Modificacion_BD
 
 from module.views import NewModule, CourseModules
 from page.views import DeletePage, NewPageModule, PageDetail, DeletePage
@@ -39,11 +35,6 @@ urlpatterns = [
 	path('<course_id>/submissions/<grade_id>/grade', GradeSubmission, name='grade-submission'),
 	#Students
 	path('<course_id>/students', StudentEnrollList, name='students'),
-<<<<<<< HEAD
-
-=======
-	path('<course_id>/students/grades', StudentGrades, name='students-grades'),
->>>>>>> Modificacion_BD
 	path('<course_id>/students/<student_id>/delete', DeleteStundentEnroll, name='delete-student'),
 	path('<course_id>/students/<student_id>/add', AddStundentEnroll, name='add-student'),
 ]
