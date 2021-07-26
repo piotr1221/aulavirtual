3dbce06c94a952f00ebd7b3282cf1f0dffcf1155
Xocrona <guilyamon_ky@hotmail.com>
2021-07-25 22:40:39 -0500

    Form validacion

--- classroom/forms.py
+++ classroom/forms.py
@@ -6,10 +6,10 @@ from classroom.models import Course, Category
 
 class NewCourseForm(forms.ModelForm):
 	picture = forms.ImageField(required=True)
-	title = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate'}), required=True)
-	description = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate'}), required=True)
+	title = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate', 'required':'true'}), required=True)
+	description = forms.CharField(widget=forms.TextInput(attrs={'class': 'validate', 'required':'true'}), required=True)
 	category = forms.ModelChoiceField(queryset=Category.objects.all())
-	syllabus = forms.CharField(widget=CKEditorWidget())
+	syllabus = forms.CharField()
 
 	class Meta:
 		model = Course
