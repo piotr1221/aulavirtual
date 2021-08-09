9ff361f9e2ec9a2bd709b7e20d772552d8f5735e
Xocrona <guilyamon_ky@hotmail.com>
2021-08-08 19:05:08 -0500

    Categoria de curso

--- authy/tests.py
+++ authy/tests.py
@@ -9,6 +9,8 @@ from .views import signup, edit_profile
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.test.client import RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+
 # Create your tests here.
 
 class ProfileTest(TestCase):
@@ -20,8 +22,7 @@ class ProfileTest(TestCase):
                                             )
 
     def test_register(self):
-        req = self.factory.post('/user/signup')
-        req.user = self.user
+        req = self.factory.post('user/signup')
 
         info = {'csrfmiddlewaretoken': get_token(req),
                 'username': 'test_user',
@@ -30,10 +31,17 @@ class ProfileTest(TestCase):
                 'confirm_password': 'test_user',
                 'action': '',
                 }
-        
+
+        user = User.objects.create_user('test_user', 'test_user@gmail.com', 'test_user')
+
+        middleware = SessionMiddleware()
+        middleware.process_request(req)
+        req.session.save()
+
         q = QueryDict('', mutable=True)
         q.update(info)
         req.POST = q
+        req.user = user
         signup(req)
         user = User.objects.get(username='test_user')
         assert user
