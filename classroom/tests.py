ce8b1b212313db4181c826f72d0aeb507e2ab040
Alexis Arias <alexis.arias555@gmail.com>
2021-08-15 17:52:28 -0500

    archivo test de classroom

---
+++ classroom/tests.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from .models import Category, Course, Grade
+from datetime import datetime
+from django.core.handlers.wsgi import WSGIRequest
+from io import BytesIO
+from django.http.request import QueryDict
+from django.middleware.csrf import get_token
+from .views import new_course
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.test.client import RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+import urllib
+from django.core.files.uploadedfile import SimpleUploadedFile
+from PIL import Image
+from django.utils.datastructures import MultiValueDict
+# Create your tests here.
+
+class CourseTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.category = Category.objects.create(title='Idiomas',
+                                                        icon='noicon',
+                                                        slug='test',
+                                                        )
+        self.user = User.objects.create_user(username='xocrona',
+                                            email='xocrona@xocrona.com',
+                                            password='xocrona',
+                                            )
+
+    def test_new_course(self):
+        req = self.factory.post('course/newcourse/')
+        req.user = self.user
+        #result = urllib.urlopen('https://static.wikia.nocookie.net/nekos-judios/images/1/17/Xocron-_digo_digo%2C_el_tio_anthony.jpg/revision/latest/top-crop/width/360/height/450?cb=20180624223121&path-prefix=es')
+        im = Image.open(r"D:\Alexis\Projects in programming\Project in Phyton\django projects\aulavirtual\media\user_1\profile.jpg")
+        buf = BytesIO()
+        #Don't forget to delete the code added in views
+        im.save(buf, format='JPEG')
+        byte_im = buf.getvalue()
+        file_data = {'profile': SimpleUploadedFile('profile.jpg', byte_im)}
+        info = {'csrfmiddlewaretoken': get_token(req),
+                'title': 'English',
+                'category': self.category.id,
+                'day': 'LU',
+                'description': 'Basic Intermediate Advance English course',
+                'time_start': '12:00:00',
+                'time_end': '14:30:00',
+                'syllabus': 'syllabus test',
+                'action': ''
+                }       
+        q = QueryDict('', mutable=True)
+        q.update(info)
+        req.POST = q
+        m = MultiValueDict('')
+        m.appendlist(file_data)
+        print(type(req.FILES))
+        req.FILES = m
+        new_course(req)
+
+        course = Course.objects.get(title = 'English')
+        assert course
+
